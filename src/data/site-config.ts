54f2acb6feb683530bc982a4384ec8ecf383c508
daridev <darideveloper@gmail.com>
2026-04-04 20:14:28 -0600

    feat(contact): update Google Maps embed location

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -55,8 +55,8 @@ export const SOCIAL_LINKS = {
 // GOOGLE MAPS
 // =============================================================================
 export const GOOGLE_MAPS = {
-    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.819467106203!2d-86.87743258896505!3d21.039908387314313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4c2b0d3f1e5d23%3A0xfd13b111ff1bfe5c!2sCancun%20Airport%20Shuttle!5e0!3m2!1ses-419!2smx!4v1722358248608!5m2!1ses-419!2smx',
-    placeId: '0x8f4c2b0d3f1e5d23:0xfd13b111ff1bfe5c',
+    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.5966047271118!2d-86.85725502440039!3d21.12864288433976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4c2bb0419c6491%3A0x145800cb5703a989!2sCancun%20Airport%20Transportation!5e0!3m2!1ses-419!2smx!4v1721079619885!5m2!1ses-419!2smx',
+    placeId: '0x8f4c2bb0419c6491:0x145800cb5703a989',
     coordinates: {
         lat: 21.12864288433976,
         lng: -86.85725502440039,
